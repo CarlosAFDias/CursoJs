0d90ed4f668079ef0c3bb4aad3f5cbef7a2079c7
CarlosAFDias <carlosalbertogeonot@yahoo.com.br>
2019-08-17 15:03:25 -0300

    Meu primeiro commit de Java Script e programação

---
+++ Aula1.js
@@ -0,0 +1,27 @@
+console.log("Nada de olá mundo hj");
+console.warn("Eu sou um aleta de Erro");
+console.error("Eu sou erro");
+
+var idade = 12;
+var nome = "Carlos"
+var salario = 980.99; //double ou float
+var relatorio = nome + " tem " + idade + " anos";
+var relatorio = ${nome} tem ${idade};
+var pais = ["Dionizio" , "Dona Nilza"];
+var veiculo = { //objeto
+    tipo: "motocileta",
+    anos:2012,
+    marca:"honda",
+    valor: 1000.99}
+
+
+var relatorio = `${nome} filho de ${pais[0]} e ${pais[1]} tem anos e recebe ${salario} de salario. compro
+console.log(idade);
+console.log(nome);
+console.log(salario);
+console.log(relatorio);
+console.log(pais.length);
+console.log(veiculo);
+console.log(namoradas);
+console.log(objetoTeste);
+alert("Agora eu programo em JS");
